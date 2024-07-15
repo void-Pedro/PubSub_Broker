2bce4a995feb1663d26401a8590d9c1f11e303d2
blackJap03 <99161267+blackJap03@users.noreply.github.com>
2024-07-15 20:02:55 -0300

    publisher.py final

--- src/publisher.py
+++ src/publisher.py
@@ -1,7 +1,18 @@
+# T1 - Sistemas Distribuídos: Videoconferência
+# Professor Doutor Fredy João Valente
+
+# Integrantes do Grupo: 
+# Bruno de Silveira Biaziolli - 760318
+# Pedro Henrique Borges - 804071
+# Rodrigo Takizawa Yamauchi - 800226
+# Vinicius Marques Rodrigues - 790717
+# 15/07/2024
+
 import zmq
 import pyaudio
 import time
 import sys
+import cv2
 
 CHUNK = 1024
 RATE = 44100
@@ -10,12 +21,14 @@ context = zmq.Context()
 socket = context.socket(zmq.PUB)
 socket.connect("tcp://localhost:5555")
 
+# Função mandar texto
 def send_text():
     while True:
-        message = "texto This is a text message"
+        message = "texto"
         socket.send_string(message)
         time.sleep(1)
-    
+
+# Função mandar áudio    
 def send_audio():
     audio = pyaudio.PyAudio()
     stream = audio.open(format=audio.get_format_from_width(2),
@@ -34,6 +47,24 @@ def send_audio():
         stream.close()
         audio.terminate()
 
+# Função mandar vídeo
+def send_video():
+    captureVideo = cv2.VideoCapture(0)
+    try:
+        while captureVideo.isOpened():
+            ret, frame = captureVideo.read()
+            if not ret:
+                break
+            _, buffer = cv2.imencode('.jpg', frame)
+            socket.send(buffer)
+            time.sleep(0.04)  # Aproximadamente 25 fps (1/0.04)
+    except KeyboardInterrupt:
+        print("Stopping...")
+    finally:
+        captureVideo.release()
+
+# Escolher via comentário 
 if __name__ == "__main__":
     # send_text()
-    send_audio()
+    # send_audio()
+    send_video()
